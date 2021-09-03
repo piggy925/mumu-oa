468fea4d0dc95dafc69f2a1e05b9a2ee141fa6e9
MuMu <piggy925@163.com>
2021-09-04 03:36:35 +0800

    feat：精简服务器返回的异常内容

---
+++ src/main/java/com/example/mumuoa/config/ExceptionAdvice.java
@@ -0,0 +1,33 @@
+package com.example.mumuoa.config;
+
+import com.example.mumuoa.exception.MumuoaException;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.shiro.authz.UnauthorizedException;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@Slf4j
+@RestControllerAdvice
+public class ExceptionAdvice {
+    @ResponseBody
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    @ExceptionHandler(Exception.class)
+    public String exceptionHandler(Exception e) {
+        log.error("执行异常", e);
+        if (e instanceof MethodArgumentNotValidException) {
+            MethodArgumentNotValidException exception = (MethodArgumentNotValidException) e;
+            return exception.getBindingResult().getFieldError().getDefaultMessage();
+        } else if (e instanceof MumuoaException) {
+            MumuoaException exception = (MumuoaException) e;
+            return exception.getMsg();
+        } else if (e instanceof UnauthorizedException) {
+            return "用户没有相关权限";
+        } else {
+            return "服务器异常";
+        }
+    }
+}
--- src/main/java/com/example/mumuoa/controller/TestController.java
+++ src/main/java/com/example/mumuoa/controller/TestController.java
@@ -4,6 +4,7 @@ import com.example.mumuoa.common.util.R;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,7 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController {
     @ApiOperation("测试方法")
     @GetMapping("/say")
-    public R sayHello() {
+    public R sayHello(@RequestBody String name) {
         return R.success().put("message", "hello world");
     }
 }
